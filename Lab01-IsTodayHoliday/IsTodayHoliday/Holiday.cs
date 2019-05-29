c453be8fcd57aaa8eea29686825495c62cad8753
Ouch.Liu <ouch.liu@xuenn.com>
2019-05-29 14:54:43 +0800

    First commit

---
+++ Lab01-IsTodayHoliday/IsTodayHoliday/Holiday.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IsTodayHoliday
+{
+    public class Holiday
+    {
+        public string GetTheme()
+        {
+            var today = DateTime.Today;
+            return ( today.Year == 2019 && today.Month == 6 && today.Day == 7 ) ? "theme.dragon-boat-festival" : "normal";
+        }
+
+    }
+}
